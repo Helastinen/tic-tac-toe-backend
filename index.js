721e77e1eecab1425fb460b5ae5b809d7de6a87d
Antti <hellsten.antti@gmail.com>
2025-12-05 15:46:44 +0200

    fixed API's to be compatible with frontend, added cors support

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const gameStats = require ("./gameStats.js");
 
 const app = express();
 app.use(express.json());
 app.use(morgan("tiny"));
+app.use(cors());
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello world</h1>");
 });
 
-app.get("/api/gamestats", (request, response) => {
-  response.json(gameStats);
+app.get("/api/gamehistory", (request, response) => {
+  response.json(gameStats.gameHistory);
+});
+
+app.get("/api/totalstats", (request, response) => {
+  response.json(gameStats.totalStats);
 });
 
 const generateId = () => {
@@ -22,20 +28,29 @@ const generateId = () => {
   return maxId + 1;
 };
 
-app.post("/api/gamestats", (request, response) => {
-  const gameStat = request.body;
-  console.log("request.body: ", gameStat);
+app.post("/api/gamehistory", (request, response) => {
+  const gameResult = request.body;
+  console.log("request.body: ", gameResult);
 
-  if (!gameStat.status) {
+  if (!gameResult.status) {
     return response.status(400).json({ error: "status missing" });
   }
 
-  gameStat.id = generateId();
-  console.log("gameStat with id: ", gameStat);
+  gameResult.id = generateId();
+  console.log("gameResult with id: ", gameResult);
 
-  gameStats.gameHistory = gameStats.gameHistory.concat(gameStat);
+  gameStats.gameHistory = gameStats.gameHistory.concat(gameResult);
 
-  response.json(gameStat);
+  response.json(gameResult);
+});
+
+app.put("/api/totalstats", (request, response) => {
+  const updatedTotalStats = request.body;
+  console.log("request.body: ", updatedTotalStats);
+
+  gameStats.totalStats = updatedTotalStats;
+
+  response.json(updatedTotalStats);
 });
 
 const unknownEndpoint = (request, response) => {
